fbd52216b98c4367741670f208e4720305e2d655
sopka13 <sopka13@mail.ru>
2022-01-31 17:01:50 +0300

    123

--- git/srcs/requirements/mariadb/tools/start.sh
+++ git/srcs/requirements/mariadb/tools/start.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-service mysql start
+service mariadb start
 
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`"
-mysql -e "CREATE USER IF NOT EXISTS \`${WP_USER}\`@'%' IDENTIFIED BY '${WP_USER_PASSWORD}'"
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO \`${WP_ADMIN}\`@'%' IDENTIFIED BY '${WP_ADMIN_PASSWORD}'"
-mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${WP_USER}\`@'%'"
-mysql -e "FLUSH PRIVILEGES"
+mariadb -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`"
+mariadb -e "CREATE USER IF NOT EXISTS \`${WP_USER}\`@'%' IDENTIFIED BY '${WP_USER_PASSWORD}'"
+mariadb -e "GRANT ALL PRIVILEGES ON *.* TO \`${WP_ADMIN}\`@'%' IDENTIFIED BY '${WP_ADMIN_PASSWORD}'"
+mariadb -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${WP_USER}\`@'%'"
+mariadb -e "FLUSH PRIVILEGES"
 
-service mysql stop
+service mariadb stop
 
-exec /usr/sbin/mysqld -u root
+exec /usr/sbin/mariadb -u root
